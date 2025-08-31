ea2c22351ecfb25e7ccc8699c0545637ca022a1c
Gianluca Orpello <g.orpello@gmail.com>
2025-08-31 11:57:13 +0200

    Initial Commit

---
+++ CarHelpGPT/CarHelpGPTApp.swift
@@ -0,0 +1,15 @@
+//
+//  Created by Gianluca Orpello.
+//  Copyright © 2025 Unicorn Donkeys. All rights reserved.
+//  
+
+import SwiftUI
+
+@main
+struct CarHelpGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
